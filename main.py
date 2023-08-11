7ef4f549caa49cbde0bae4e9b1c5059b13670369
amnasyed1 <amna.syed@stonybrook.edu>
2023-08-11 20:35:29 +0000

    added a requirements.txt file

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # import module1
 # import module1 as mod1
 # from module1 import *
-from module1 import addition, subtraction
 
-output1= addition(5, 10)
-output2 = subtraction(5, 10)
+import sys
+
+sys.path.append('/home/amna_syed/hha_506_class9/modules')
+
+import module1
+
+output1= module1.addition(5, 10)
+output2 = module1.subtraction(5, 10)
 
 print('Output: ', output1) 
 print('Output: ', output2)
--- systemChecks.py
+++ systemChecks.py
@@ -1,13 +1,4 @@
-import sys as mysystem
+import pandas as pd
+import sqlalchemy
 
-print(mysystem.path)
-
-output =[
-    '/home/amna_syed/hha_506_class9', 
-    '/usr/lib/python39.zip',
-    '/usr/lib/python3.9',
-    '/usr/lib/python3.9/lib-dynload',
-    '/home/amna_syed/.local/lib/python3.9/site-packages',
-    '/usr/local/lib/python3.9/dist-packages',
-    '/usr/lib/python3/dist-packages'
-]
+dataframe = pd.read_csv('')
