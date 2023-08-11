a4d78bc94cdc73d7176c163dc64097b6a534416d
amnasyed1 <amna.syed@stonybrook.edu>
2023-08-11 19:55:29 +0000

    added some notes on modules, aliases

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import module1
+# import module1
+# import module1 as mod1
+# from module1 import *
+from module1 import addition, subtraction
 
-output1= module1.addition(5, 10)
-output2 = module1.subtraction(5, 10)
+output1= addition(5, 10)
+output2 = subtraction(5, 10)
 
 print('Output: ', output1) 
 print('Output: ', output2)
---
+++ systemChecks.py
@@ -0,0 +1,13 @@
+import sys as mysystem
+
+print(mysystem.path)
+
+output =[
+    '/home/amna_syed/hha_506_class9', 
+    '/usr/lib/python39.zip',
+    '/usr/lib/python3.9',
+    '/usr/lib/python3.9/lib-dynload',
+    '/home/amna_syed/.local/lib/python3.9/site-packages',
+    '/usr/local/lib/python3.9/dist-packages',
+    '/usr/lib/python3/dist-packages'
+]
